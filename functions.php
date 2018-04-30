4efcb397c7ac8f0dd8821f46369668e5435c65a3
Thiago Senna <trsenna@gmail.com>
2018-04-30 17:16:33 -0300

    code cleanup/format

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 namespace site\tachyons;
 
-add_action( 'wp_enqueue_scripts', function() {
-  wp_enqueue_style( 'tachyons-theme', get_parent_theme_file_uri( 'runtime/style.theme.css' ) );
-  wp_enqueue_style( 'tachyons-style', get_stylesheet_uri() );
-});
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style( 'tachyons-theme', get_parent_theme_file_uri( 'runtime/style.theme.css' ) );
+    wp_enqueue_style( 'tachyons-style', get_stylesheet_uri() );
+} );
